0de934fed7c61b58eca2392b8197002c4c3f9a56
Douglas Cassiano de Moura <douuuglas@gmail.com>
2021-02-28 09:44:34 -0300

    Criação da api de taxa de juros

---
+++ SP.TAXA.JUROS.IntegrationTests/Controller/TaxaJurosControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SP.TAXA.JUROS.IntegrationTests.Controller
+{
+    public class TaxaJurosControllerTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public TaxaJurosControllerTests()
+        {
+            _httpClient = new WebApplicationFactory<Startup>().CreateClient();
+        }
+
+        [Fact]
+        public async Task DadaRequiscaoGetDeveRetornarUmPorcento()
+        {
+            var endpoint = "TaxaJuros";
+
+            var response = await _httpClient.GetAsync(endpoint);
+            var result = Convert.ToDecimal(await response.Content.ReadAsStringAsync());
+
+            response.StatusCode.Should().Be(200);
+            result.Should().Be(0.01m);
+        }
+    }
+}
---
+++ SP.TAXA.JUROS/Controllers/TaxaJurosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SP.TAXA.JUROS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    /// <summary>
+    /// Controller responsável por retornar as taxas de juros
+    /// </summary>
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        /// <summary>
+        /// Método responsável por retorna a taxa de juros
+        /// </summary>
+        /// <returns>
+        /// Taxa de juros fixa no valor de 1%
+        /// </returns>
+        public decimal Get()
+        {
+            return 0.01m;
+        }
+    }
+}
